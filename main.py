296c91ab26705f86070272cc1b6bfd407151cfca
CompileNix <compilenix@gmail.com>
2020-01-12 21:19:26 +0100

    add todo's

--- main.py
+++ main.py
@@ -4,4 +4,18 @@ proxmox = ProxmoxAPI('x.x.x.x', user='root@pam', password='****************', ve
 
 for node in proxmox.nodes.get():
     for vm in proxmox.nodes(node['node']).qemu.get():
-        print("vm {0} on {1} named {2} is {3}" .format(vm['vmid'], node['node'], vm['name'], vm['status']))
+        config = proxmox.nodes(node['node']).qemu(vm['vmid']).config.get()
+        print("vm {0} on {1} named {2} is {3} with confsig {4}" .format(vm['vmid'], node['node'], vm['name'], vm['status'], config))
+
+# TODO: read backup jobs
+# TODO: get nodes
+# TODO: get rbd storage's
+# TODO: get vm config's
+# TODO: get vm rbd disk's
+# TODO: create and mount metadata of vm in backup cluster
+# TODO: copy current vm config to metadata
+# TODO: unmount metadata
+# TODO: create snapshot of metadata
+# TODO: create vm snapshot
+# TODO: backup rbd disk's of vm
+# TODO: remove old vm snapshot
